e57daa2beefc322d51c581f2845710a610536446
Jan Techner <jantechner@live.com>
2018-11-22 15:30:11 +0100

    Dodanie klasy JSONReader
    Klasa JSONReader zawiera metody przetwarzania obiektów w formacie JSON

--- src/main/java/pl/put/poznan/transformer/logic/Scenario.java
+++ src/main/java/pl/put/poznan/transformer/logic/Scenario.java
@@ -1,11 +1,7 @@
 package pl.put.poznan.transformer.logic;
-import java.util.ArrayList;
-import java.util.Collections;
 
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Klasa "Scenario"
@@ -23,73 +19,15 @@ public class Scenario {
     ScenarioHelper helper = new ScenarioHelper();
 
     /**
-     * Konstruktor obiektów klasy "Scenario"
-     * @param sendscenario Scenariusz do przetworzenia
+     * Konstruktor klasy "Scenario"
+     * @param sendscenario Scenariusz w formacie JSON wysłany przez użytkownika
      */
     public Scenario(JSONObject sendscenario) {
-
         this.scenario = sendscenario;
         this.title = sendscenario.getAsString("title");
-        this.actors = readJson("actors");
-        this.systemActors = readJson("system_actors");
-        this.steps = readJsonSteps();
-    }
-
-    /**
-     * Metoda zwraca listę wartości z danej kolekcji
-     * @param field Kolekcja, dla której wartości mają zostać odczytane
-     * @return Lista wartości kolekcji
-     */
-    private List<String> readJson(String field) {
-        List<String> list = new ArrayList<>();
-        Object obj = this.scenario.get(field);
-        if (obj instanceof String) {
-            list.add((String)obj);
-        } else if (obj instanceof ArrayList) {
-            for (Object o : (ArrayList) obj){
-                if (o instanceof String)
-                    list.add((String) o);
-            }
-        }
-        return list;
-    }
-
-    /**
-     * Metoda zwracająca listę "steps"
-     * @return Lista "steps"
-     */
-    private List<Step> readJsonSteps() {
-        Object stepsAsObject = this.scenario.get("steps");
-        List<Object> stepsAsObjectsList = new ArrayList<>();
-        if (stepsAsObject instanceof ArrayList)
-             stepsAsObjectsList.addAll((ArrayList)stepsAsObject);
-        return recursiveStepsReader(stepsAsObjectsList);
-    }
-
-    /**
-     * Rekurencyjne odczytywanie (przetworzenie) "steps"
-     * @param steps Lista obiektów z kolekcji "steps"
-     * @return Przetworzona lista "steps"
-     */
-    private List<Step> recursiveStepsReader(List<Object> steps) {
-        List<Step> steplist = new ArrayList<>();
-        for (int i=0; i<steps.size(); i++)
-            if (steps.get(i) instanceof String) {
-                if (i == steps.size() - 1) {
-                    steplist.add(new Step((String) steps.get(i), null));
-                } else {
-                    if (steps.get(i + 1) instanceof String) {
-                        steplist.add(new Step((String) steps.get(i), null));
-                    } else if (steps.get(i + 1) instanceof ArrayList) {
-                        List<Object> substepsAsObjectList = new ArrayList<>();
-                        if (steps.get(i + 1) instanceof ArrayList)
-                            substepsAsObjectList.addAll((ArrayList) steps.get(i + 1));
-                        List<Step> substeps = recursiveStepsReader(substepsAsObjectList);
-                        steplist.add(new Step((String) steps.get(i), substeps));
-                    }
-                }
-            }
-        return steplist;
+        this.actors = JSONReader.getProperty(scenario, "actors");
+        this.systemActors = JSONReader.getProperty(scenario, "system_actors");
+        this.steps = JSONReader.getStepsRecursively(JSONReader.getObjectsList(scenario, "steps"));
     }
 
 
