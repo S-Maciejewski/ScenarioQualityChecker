8d2f591b704714bbb388cf3928654f1cf6dbf45f
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-11-27 12:16:29 +0100

    Api CORS fixes

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -11,6 +11,7 @@ public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(path = "/{function}", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public JSONObject post(@PathVariable String function, @RequestBody JSONObject sendedScenario) {
 
@@ -24,6 +25,7 @@ public class TextTransformerController {
         return FunctionTrigger.run(scenario, function, 0);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(path = "/showScenario/{maxDepth}", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public JSONObject post(@PathVariable int maxDepth, @RequestBody JSONObject sendedScenario) {
 
